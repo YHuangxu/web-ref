5c42099d899f2f08b9deab654b531d68a3fd8155
YHuangxu <46688089+YHuangxu@users.noreply.github.com>
2019-02-28 22:39:38 -0800

    Update keys.js
    security suggestions

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,5 @@ module.exports = {
 	secretOrKey: 'secret',
 	mongoURI: 'mongodb://admin1:admin1@ds255005.mlab.com:55005/web-ref-app'
 };
+
+//YHuangxu: this file should be included in .gitignore to keep the db access safe
